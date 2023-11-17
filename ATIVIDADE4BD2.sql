aefa75bc97f05850a08fec848681a3c578bb6791
Pedro-Costa3640 <134285844+Pedro-Costa3640@users.noreply.github.com>
2023-11-17 11:18:07 -0300

    Update ATIVIDADE4BD2.sql

--- ATIVIDADE4BD2.sql
+++ ATIVIDADE4BD2.sql
@@ -1,29 +1,60 @@
 create database Atv4_2;
 use Atv4_2;
 
-CREATE TABLE Funcionarios(
-    Id_funcionario INT PRIMARY KEY AUTO_INCREMENT,
-    Nome VARCHAR(100),
-    Data_admissao DATE
+create table Produtos(
+    Id_produto int primary key auto_increment,
+    Nome varchar(255) not null,
+    Quantidade_estoque int not null
+);
+
+create table Vendas(
+    Id_venda int primary key auto_increment,
+    Data_venda datetime not null
+);
+
+create table Itens_venda (
+    Id_item_venda int primary key,
+    Id_venda int not null,
+    Id_produto int not null,
+    Quantidade int not null,
+    foreign key (id_venda) references vendas(id_venda),
+    foreign key (id_produto) references produtos(id_produto)
 );
 
 DELIMITER //
-CREATE TRIGGER checar_data_admissao
-BEFORE INSERT ON funcionarios
+
+CREATE TRIGGER verificar_estoque
+BEFORE INSERT ON Itens_venda
 FOR EACH ROW
-	BEGIN
-	IF NEW.data_admissao <= CURDATE() THEN
-	SIGNAL SQLSTATE '45000'
-	SET MESSAGE_TEXT = 'Data de admissão não pode ser igual a data atual. Tente Novamente';
-	END IF;
+BEGIN
+    DECLARE estoque_disponivel INT;
+
+    -- Obtém a quantidade em estoque do produto correspondente
+    SELECT Quantidade_estoque INTO estoque_disponivel
+    FROM Produtos
+    WHERE Id_produto = NEW.Id_produto;
+
+    -- Verifica se a quantidade em estoque é suficiente
+    IF estoque_disponivel < NEW.Quantidade THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Produto está fora de estoque. Quantidade disponível: ' || CAST(estoque_disponivel AS CHAR);
+    END IF;
 END;
+
 //
+
 DELIMITER ;
 
-INSERT INTO Funcionarios (Nome, Data_admissao)
-VALUES ('João Kleber', '2022-10-03'),
-('Mariana Lorenço', '2021-12-15'),
-('Patrick', '2020-10-20'),
-('Jordan', '2023-06-29'); 
 
-SELECT * FROM Funcionarios;
+insert into Produtos (Nome, Quantidade_estoque) 
+values ('Raquete de Tênis', 20),
+('Raquete de Baisebol', 55),
+('Raquete de seilá', 30);
+
+insert into Vendas (Data_venda) 
+values ('2023-06-24 19:20:00');
+
+insert into Itens_venda (Id_venda, Id_produto, Quantidade) 
+values (1, 1, 25);
+
+select * from Produtos;
